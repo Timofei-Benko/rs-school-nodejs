9a17b0746e99c44f5a820c9c2a37b62bf5ceb2d0
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-16 21:26:28 +0300

    chore: change default values in constructor

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -3,8 +3,8 @@ const {v4: uuid} = require('uuid');
 class User {
   constructor({
     id = uuid(),
-    name = 'USER',
-    login = 'user',
+    name = 'TEST_USER',
+    login = 'TEST_LOGIN',
     password = 'P@55w0rd'
   } = {}) {
     this.id = id;
