90621e5493bc3ce52e8b500e00acecd026c8c795
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-16 19:09:54 +0300

    feat: set up user service

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,15 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const getUser = (id) => usersRepo.getUser(id);
+const createUser = (userData) => usersRepo.createUser(userData);
+const updateUser = (id, newUserData) => usersRepo.updateUser(id, newUserData);
+const deleteUser = (id) => usersRepo.deleteUser(id);
 
-module.exports = { getAll };
+module.exports = {
+    getAll,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser
+};
