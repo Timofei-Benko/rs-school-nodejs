de6cd30c3fd44a21df5fa07a39204500bfc6c528
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-16 19:10:20 +0300

    feat: add user routes

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -3,9 +3,31 @@ const User = require('./user.model');
 const usersService = require('./user.service');
 
 router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
+      const users = await usersService.getAll();
+      res.json(users.map(User.toResponse));
 });
+
+router.route('/:id').get(async (req, res) => {
+      const user = await usersService.getUser(req.params.id)
+      res.status(200).send(User.toResponse(user));
+});
+
+router.route('/').post(async (req, res) => {
+      const user = await usersService.createUser(User.fromRequest(req.body));
+      res.status(201).send(User.toResponse(user));
+});
+
+router.route('/:id').put(async (req, res) => {
+      const user = await usersService.updateUser(
+          req.params.id,
+          req.body
+      );
+      res.status(200).send(User.toResponse(user));
+});
+
+router.route('/:id').delete(async (req, res) => {
+      const user = await usersService.deleteUser(req.params.id);
+      res.status(204).send(User.toResponse(user));
+})
 
 module.exports = router;
