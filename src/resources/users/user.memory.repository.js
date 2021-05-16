e27a4207db972b496099e6d6f29d875f9deb6fc3
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-16 19:09:00 +0300

    feat: add user interaction methods

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,33 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const User = require('./user.model');
+
+const users = [new User()];
+
+const getAll = async () => users;
+
+const getUser = async (id) => users.find(user => user.id === id);
+
+const createUser = async (user) => {
+  users.push(new User(user));
+  return user
 };
 
-module.exports = { getAll };
+const updateUser = async (id, newUserData) => {
+  const userIndex = users.findIndex(user => user.id === id);
+  const newUser = {...newUserData, id};
+  users.splice(userIndex, 1, newUser);
+  return newUser;
+};
+
+const deleteUser = async (id) => {
+  const userToDelete = users.find(user => user.id === id);
+  users.splice(users.indexOf(userToDelete), 1);
+  return userToDelete;
+};
+
+module.exports = {
+  getAll,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+};
