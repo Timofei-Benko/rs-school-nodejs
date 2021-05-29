c11c567bfe5ec4e74881d173d756d09beb2cbb81
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-30 01:10:18 +0300

    refactor: change file extension & make adjustments for ts compiler

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 const { PORT } = require('./common/config');
-const app = require('./app');
+const appToRun = require('./app');
 
-app.listen(PORT, () =>
+appToRun.listen(PORT, () =>
   console.log(`App is running on http://localhost:${PORT}`)
 );
