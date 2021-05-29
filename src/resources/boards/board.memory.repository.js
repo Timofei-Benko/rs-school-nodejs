25f1b9567eb07af5d95e72bf2b3afe36270f945f
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-29 20:37:25 +0300

    refactor: include changes to board storage methods jsdoc

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -6,22 +6,22 @@ const { deleteAllTasks } = require('../tasks/task.service');
 
 /**
  * Returns all boards from the boards storage in an array as a promise.
- * @returns {Promise<*[]>}
+ * @returns {Promise<object[]>} Array of board objects
  */
 const getAll = async () => boards;
 
 /**
  * Finds a board object based on the given id and returns it as a promise.
- * @param {string} id
- * @returns {Promise<*{}>}
+ * @param {string} id Board id
+ * @returns {Promise<object>} Found board's data
  */
 const getBoard = async (id) => boards.find(board => board.id === id);
 
 /**
  * Creates new board object based on the provided object and pushes it to board storage.
  * Returns new board object as a promise.
- * @param {object} boardData
- * @returns {Promise<*{}>}
+ * @param {object} boardData Board data
+ * @returns {Promise<object>} Created board's data
  */
 const createBoard = async (boardData) => {
   const newBoard = new Board(boardData);
@@ -32,9 +32,9 @@ const createBoard = async (boardData) => {
 /**
  * Finds board object based on the id and updates it in the boards storage based on the provided object.
  * Returns updated board object as a promise.
- * @param {string} id
- * @param {object} newBoardData
- * @returns {Promise<*{}>}
+ * @param {string} id Board id
+ * @param {object} newBoardData Board data to update
+ * @returns {Promise<object>} Updated board's data
  */
 const updateBoard = async (id, newBoardData) => {
   const boardToUpdate = await getBoard(id);
@@ -46,7 +46,7 @@ const updateBoard = async (id, newBoardData) => {
 /**
  * Finds board object based on the id, deletes all of its tasks
  * and removes the board object from boards storage.
- * @param {string} boardId
+ * @param {string} boardId Board id
  * @returns {Promise<void>}
  */
 const removeBoard = async (boardId) => {
