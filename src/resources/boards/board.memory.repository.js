a6c2d8e22721bbcacef32783038f810a7e959a65
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-29 13:16:10 +0300

    feat: add jsdoc for board storage methods

--- src/resources/boards/board.memory.repository.js
+++ src/resources/boards/board.memory.repository.js
@@ -4,16 +4,38 @@ const boards = [];
 
 const { deleteAllTasks } = require('../tasks/task.service');
 
+/**
+ * Returns all boards from the boards storage in an array as a promise.
+ * @returns {Promise<*[]>}
+ */
 const getAll = async () => boards;
 
+/**
+ * Finds a board object based on the given id and returns it as a promise.
+ * @param {string} id
+ * @returns {Promise<*{}>}
+ */
 const getBoard = async (id) => boards.find(board => board.id === id);
 
+/**
+ * Creates new board object based on the provided object and pushes it to board storage.
+ * Returns new board object as a promise.
+ * @param {object} boardData
+ * @returns {Promise<*{}>}
+ */
 const createBoard = async (boardData) => {
   const newBoard = new Board(boardData);
   boards.push(newBoard);
   return newBoard;
 };
 
+/**
+ * Finds board object based on the id and updates it in the boards storage based on the provided object.
+ * Returns updated board object as a promise.
+ * @param {string} id
+ * @param {object} newBoardData
+ * @returns {Promise<*{}>}
+ */
 const updateBoard = async (id, newBoardData) => {
   const boardToUpdate = await getBoard(id);
   const updatedBoard = new Board({...boardToUpdate, ...newBoardData });
@@ -21,6 +43,12 @@ const updateBoard = async (id, newBoardData) => {
   return updatedBoard;
 };
 
+/**
+ * Finds board object based on the id, deletes all of its tasks
+ * and removes the board object from boards storage.
+ * @param {string} boardId
+ * @returns {void}
+ */
 const removeBoard = async (boardId) => {
   const boardIndex = boards.findIndex(board => board.id === boardId);
   await deleteAllTasks(boardId);
