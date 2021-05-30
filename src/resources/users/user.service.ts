1e3e21f11b6505a0e7016e2c19d981b42126e115
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-05-30 16:37:31 +0300

    refactor: impoted user interface to type function params

--- src/resources/users/user.service.ts
+++ src/resources/users/user.service.ts
@@ -1,9 +1,10 @@
 const usersRepo = require('./user.memory.repository');
+import IUser = require('./user.interface');
 
 const getAll = async (): Promise<Function> => usersRepo.getAll();
 const getUser = async (id: string): Promise<Function> => usersRepo.getUser(id);
-const createUser = async (userData: object): Promise<Function> => usersRepo.createUser(userData);
-const updateUser = async (id: string, newUserData: object): Promise<Function> => usersRepo.updateUser(id, newUserData);
+const createUser = async (userData: IUser): Promise<Function> => usersRepo.createUser(userData);
+const updateUser = async (id: string, newUserData: IUser): Promise<Function> => usersRepo.updateUser(id, newUserData);
 const deleteUser = async (id: string): Promise<Function> => usersRepo.deleteUser(id);
 
 module.exports = {
