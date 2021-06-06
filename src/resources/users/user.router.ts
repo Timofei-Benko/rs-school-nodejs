eae23b3a136d8aeeb37b0a44940272b8914b7870
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-06 22:54:13 +0300

    refactor: configure router modules to catch unhandled errors and pass them to error handler middleware

--- src/resources/users/user.router.ts
+++ src/resources/users/user.router.ts
@@ -3,32 +3,52 @@ const User = require('./user.model');
 const usersService = require('./user.service');
 import IUser = require('./user.interface');
 
-router.route('/').get(async (_req, res) => {
-  const users: Array<IUser> = await usersService.getAll();
-  res.json(users.map(User.toResponse));
+router.route('/').get(async (_req, res, next) => {
+  try {
+    const users: Array<IUser> = await usersService.getAll();
+    res.json(users.map(User.toResponse));
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.route('/:id').get(async (req, res) => {
-  const user: IUser = await usersService.getUser(req.params.id)
-  res.status(200).send(User.toResponse(user));
+router.route('/:id').get(async (req, res, next) => {
+  try {
+    const user: IUser = await usersService.getUser(req.params.id)
+    res.status(200).send(User.toResponse(user));
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.route('/').post(async (req, res) => {
-  const user :IUser = await usersService.createUser(User.fromRequest(req.body));
-  res.status(201).send(User.toResponse(user));
+router.route('/').post(async (req, res, next) => {
+  try {
+    const user: IUser = await usersService.createUser(User.fromRequest(req.body));
+    res.status(201).send(User.toResponse(user));
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.route('/:id').put(async (req, res) => {
-  const user: IUser = await usersService.updateUser(
-    req.params.id,
-    req.body
-  );
-  res.status(200).send(User.toResponse(user));
+router.route('/:id').put(async (req, res, next) => {
+  try {
+    const user: IUser = await usersService.updateUser(
+        req.params.id,
+        req.body
+    );
+    res.status(200).send(User.toResponse(user));
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.route('/:id').delete(async (req, res) => {
-  const user: IUser = await usersService.deleteUser(req.params.id);
-  res.status(204).send(User.toResponse(user));
-})
+router.route('/:id').delete(async (req, res, next) => {
+  try {
+    const user: IUser = await usersService.deleteUser(req.params.id);
+    res.status(204).send(User.toResponse(user));
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
