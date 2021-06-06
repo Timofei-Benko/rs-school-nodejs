ce6bb9fa1cb23d70c7be1362d359e5471e229bdc
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-06 17:39:19 +0300

    fix: inconsistency in creation of the logs directory

--- src/common/logger.ts
+++ src/common/logger.ts
@@ -1,11 +1,10 @@
 const fs = require('fs');
 const logger = require('morgan');
 
-if (!fs.existsSync('./logs')){
-    fs.mkdirSync('./logs');
-}
-
 const morganLogger = (): Array<typeof logger> => {
+
+    if (!fs.existsSync('./logs')) fs.mkdirSync('./logs');
+
     return [
         logger('dev'),
         logger('common', {
