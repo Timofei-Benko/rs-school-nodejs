5924204916d22dc6d5091b079d770a1ee5df1644
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-06-20 22:56:25 +0300

    feat: update app config

--- src/common/config.ts
+++ src/common/config.ts
@@ -10,5 +10,10 @@ module.exports = {
   NODE_ENV: process.env['NODE_ENV'],
   MONGO_CONNECTION_STRING: process.env['MONGO_CONNECTION_STRING'],
   JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
-  AUTH_MODE: process.env?.['AUTH_MODE']
+  AUTH_MODE: process.env?.['AUTH_MODE'],
+  POSTGRES_PORT: process.env['POSTGRES_PORT'],
+  POSTGRES_USER: process.env['POSTGRES_USER'],
+  POSTGRES_PASSWORD: process.env['POSTGRES_PASSWORD'],
+  POSTGRES_DB: process.env['POSTGRES_DB'],
+  POSTGRES_HOST: process.env['POSTGRES_HOST']
 };
