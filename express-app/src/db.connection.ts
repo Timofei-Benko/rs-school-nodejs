0252c260e2251fff32892eafd65ca6a4d6a7939c
Timofei Benko <62883560+Timofei-Benko@users.noreply.github.com>
2021-07-11 22:41:39 +0300

    Delete express-app directory

--- express-app/src/db.connection.ts
+++
@@ -1,6 +0,0 @@
-const { Connection, createConnection } = require('typeorm');
-const ormConfig = require('./common/orm.config');
-
-const connectDB = async (): Promise<typeof Connection> => createConnection(ormConfig);
-
-module.exports = connectDB;
--- express-app/src/resources/users/user.interface.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable  @typescript-eslint/no-unused-vars */
-interface IUser {
-    id: string,
-    name?: string,
-    login?: string,
-    password?: string,
-}
-
-export = IUser;
